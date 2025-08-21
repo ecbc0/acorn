bdab4e1bcfa35aaeec2d2375f5af4a0ee5f92dbd
Kevin Lacker <lacker@gmail.com>
2025-08-21 10:34:55 -0700

    refactor

--- src/goal.rs
+++ src/goal.rs
@@ -31,11 +31,14 @@ pub struct Goal {
 }
 
 impl Goal {
-    /// Creates a Goal for a block that has a goal.
-    pub fn block(env: &Environment, prop: &Proposition) -> Goal {
-        let first_line = env.first_line;
-        let last_line = env.last_line();
-
+    /// Creates a new Goal with the given parameters.
+    fn new(
+        env: &Environment,
+        prop: &Proposition,
+        proof_insertion_line: u32,
+        first_line: u32,
+        last_line: u32,
+    ) -> Goal {
         // Goals should never be generic.
         assert!(!prop.value.has_generic());
 
@@ -50,7 +53,7 @@ impl Goal {
             module_id: env.module_id,
             description,
             proposition: prop.clone(),
-            proof_insertion_line: last_line,
+            proof_insertion_line,
             insert_block: env.implicit,
             inconsistency_okay: env.includes_explicit_false,
             first_line,
@@ -58,30 +61,17 @@ impl Goal {
         }
     }
 
+    /// Creates a Goal for a block that has a goal.
+    pub fn block(env: &Environment, prop: &Proposition) -> Goal {
+        let first_line = env.first_line;
+        let last_line = env.last_line();
+        Self::new(env, prop, last_line, first_line, last_line)
+    }
+
     /// Creates a Goal for a proposition that is inside a block (or standalone).
     pub fn interior(env: &Environment, prop: &Proposition) -> Goal {
         let first_line = prop.source.range.start.line;
         let last_line = prop.source.range.end.line;
-
-        // Goals should never be generic.
-        assert!(!prop.value.has_generic());
-
-        let description = match prop.theorem_name() {
-            Some(name) => name.to_string(),
-            None => CodeGenerator::new(&env.bindings)
-                .value_to_code(&prop.value)
-                .unwrap_or("<goal>".to_string()),
-        };
-
-        Goal {
-            module_id: env.module_id,
-            description,
-            proposition: prop.clone(),
-            proof_insertion_line: first_line,
-            insert_block: env.implicit,
-            inconsistency_okay: env.includes_explicit_false,
-            first_line,
-            last_line,
-        }
+        Self::new(env, prop, first_line, first_line, last_line)
     }
 }
