96a5eba50a36a5613cbca01a7859d5302fc1b1d9
Kevin Lacker <lacker@gmail.com>
2025-09-06 08:25:14 -0700

    deref

--- src/maybe_owned.rs
+++ src/maybe_owned.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 pub enum MaybeOwned<'a, T: ?Sized> {
     Borrowed(&'a T),
     Owned(Box<T>),
@@ -5,6 +7,17 @@ pub enum MaybeOwned<'a, T: ?Sized> {
 
 impl<'a, T: ?Sized> AsRef<T> for MaybeOwned<'a, T> {
     fn as_ref(&self) -> &T {
+        match self {
+            MaybeOwned::Borrowed(t) => t,
+            MaybeOwned::Owned(b) => b,
+        }
+    }
+}
+
+impl<'a, T: ?Sized> Deref for MaybeOwned<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
         match self {
             MaybeOwned::Borrowed(t) => t,
             MaybeOwned::Owned(b) => b,
