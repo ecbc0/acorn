97783f8613e75afba251f8e1e2efc8349b3b0171
Kevin Lacker <lacker@gmail.com>
2025-09-08 16:09:30 -0700

    do a reverify

--- src/bin/profile_verifier.rs
+++ src/bin/profile_verifier.rs
@@ -1,4 +1,5 @@
 // A representative run of the verifier, to use for profiling.
+// This does a reverify run, so you should run it once first to populate the cache.
 //
 // To profile using samply:
 //
@@ -11,12 +12,14 @@ fn main() {
     let current_dir = std::env::current_dir().unwrap();
     for _ in 0..1 {
         let config = ProjectConfig::default();
-        let verifier = Verifier::new(
+        let mut verifier = Verifier::new(
             current_dir.clone(),
             config,
             Some("rat.rat_base".to_string()),
         )
         .expect("Failed to create verifier");
+        verifier.builder.reverify = true;
+        verifier.builder.check_hashes = false;
 
         let output = verifier.run().unwrap();
         if !output.status.is_good() {
