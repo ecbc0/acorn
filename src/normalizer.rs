7d6373b8ad340c7c22f389c755d92fc51fb57533
Kevin Lacker <lacker@gmail.com>
2025-10-01 19:01:09 -0700

    forall_inside_lambda

--- src/normalizer.rs
+++ src/normalizer.rs
@@ -1988,6 +1988,33 @@ mod tests {
     }
 
     #[test]
+    fn test_normalizing_forall_inside_lambda() {
+        let mut env = Environment::test();
+        env.add(
+            r#"
+            type Nat: axiom
+
+            let f: (Nat, Nat) -> Bool = axiom
+            let g: Nat -> Bool = axiom
+
+            theorem goal {
+                g = function(x: Nat) {
+                        forall(y: Nat) {
+                            f(x, y)
+                        }
+                    }
+            }
+        "#,
+        );
+        let mut norm = Normalizer::new();
+        norm.check(
+            &env,
+            "goal",
+            &["not g(x0) or f(x0, x1)", "not f(x0, s0(x0)) or g(x0)"],
+        );
+    }
+
+    #[test]
     fn test_normalizing_pre_expanded_exists_inside_lambda() {
         let mut env = Environment::test();
         env.add(
