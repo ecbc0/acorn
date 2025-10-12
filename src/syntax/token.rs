f9bedb7b942de5bfb2b588083de80653f4eb3181
ecbc <earth.carbon.based.consciousness@gmail.com>
2025-10-12 23:32:04 +0800

    make '!' usable in identifier for factorial. example: 3.! = 6

--- src/syntax/token.rs
+++ src/syntax/token.rs
@@ -570,7 +570,7 @@ impl Token {
     }
 
     pub fn identifierish(ch: char) -> bool {
-        ch.is_alphanumeric() || ch == '_'
+        ch.is_alphanumeric() || ch == '_' || ch == '!'
     }
 
     /// Extracts the content from a doc comment token.
@@ -723,7 +723,7 @@ impl Token {
                     '.' => TokenType::Dot,
                     '!' => match char_indices.next_if_eq(&(char_index + 1, '=')) {
                         Some(_) => TokenType::NotEquals,
-                        None => TokenType::Invalid,
+                        None => TokenType::Identifier,
                     },
                     '=' => TokenType::Equals,
                     '+' => TokenType::Plus,
@@ -941,7 +941,7 @@ impl TokenIter {
             TokenType::SelfToken => {}
             TokenType::Identifier => match name_token.text().chars().next() {
                 Some(c) => {
-                    if !c.is_ascii_lowercase() {
+                    if !(c.is_ascii_lowercase() || c == '!') {
                         return Err(name_token.error("invalid variable name"));
                     }
                 }
