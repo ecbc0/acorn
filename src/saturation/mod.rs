bb71f212811f7837bce6adee29d65ffbaef40886
Kevin Lacker <lacker@gmail.com>
2025-11-12 15:30:24 -0800

    rename the saturation prover file

--- src/saturation/mod.rs
+++ src/saturation/mod.rs
@@ -4,11 +4,11 @@ pub mod features;
 pub mod fingerprint;
 pub mod passive_set;
 pub mod proof;
-pub mod prover;
 pub mod rewrite_tree;
+pub mod saturation_prover;
 pub mod score;
 pub mod scorer;
 pub mod scoring_model;
 
 // Re-export the main public types
-pub use prover::SaturationProver;
+pub use saturation_prover::SaturationProver;
