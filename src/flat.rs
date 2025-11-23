4be1bcf29b334d2b76cf16c02fadb54148c73207
Kevin Lacker <lacker@gmail.com>
2025-11-23 08:44:59 -0800

    DataType

--- src/flat.rs
+++ src/flat.rs
@@ -1,3 +1,5 @@
+use crate::term::TypeId;
+
 /// A flattened representation of a term that can include full type information.
 pub enum FlatComponent {
     /// A TermHeader indicates that the following regions of the buffer make up a term.
@@ -7,4 +9,7 @@ pub enum FlatComponent {
         num_type_components: u16,
         num_value_components: u16,
     },
+
+    /// A DataType is a single FlatComponent that represents a type.
+    DataType(TypeId),
 }
